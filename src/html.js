cbad097f712f764bfecbc708ec831a064ddbdca6
darkwind666 <darkwinddev@gmail.com>
2020-10-26 03:27:27 +0300

    add apple business chat

--- src/html.js
+++ src/html.js
@@ -12,6 +12,22 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <script src="//code-ya.jivosite.com/widget/JPf4ILfWPy" async></script>
+        <script src="https://static.cdn-apple.com/businesschat/start-chat-button/2/index.js"></script>
+        <div class="apple-business-chat-banner-container contacts__button" 
+        data-apple-business-id="26de39d6-f978-4f56-853e-915d189b2876" 
+        data-apple-business-phone="+12163954902"
+        data-apple-banner-cta="Questions? We can help." 
+        data-apple-banner-context="Tap to chat with our support now" 
+        data-apple-banner-orientation="vertical" 
+        data-apple-banner-rounded-corners="false" 
+        data-apple-icon-color="#fff" 
+        data-apple-icon-background-color="rgb(255, 255, 255)"
+        data-apple-banner-background-color="rgb(20, 24, 33)" 
+        data-apple-banner-text-color="rgb(255, 255, 255)" 
+        data-apple-banner-icon-background-color="rgb(68, 187, 110)" 
+        data-apple-banner-icon-color="rgba(255, 255, 255, 255)" 
+        data-apple-banner-scale="1"
+        ></div>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
